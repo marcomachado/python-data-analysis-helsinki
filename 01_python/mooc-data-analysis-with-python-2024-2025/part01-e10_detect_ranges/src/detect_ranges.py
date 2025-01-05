a3c22512a71ab3b46a92d1432f4b772da4da4345
marcomachado <marco.santos.machado@gmail.com>
2025-01-05 20:07:37 -0300

    python review

--- 01_python/mooc-data-analysis-with-python-2024-2025/part01-e10_detect_ranges/src/detect_ranges.py
+++ 01_python/mooc-data-analysis-with-python-2024-2025/part01-e10_detect_ranges/src/detect_ranges.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
 
 def detect_ranges(L):
-    return []
+    L.sort()
+    print(L)
+    n = 0
+    newList = []
+    while n < len(L)-1:
+        print(f"n={n}")
+        atual = L[n]
+        next = L[n+1]
+        if next != atual+1:            
+            newList.append(L[n])
+            n += 1
+            
+            
+        else:
+            indexInicial = n
+            numInicial = atual
+            print("else")
+            while next == atual+1 and n < len(L)-1:            
+                print("while")
+                atual = L[n]
+                next = L[n+1]
+                n += 1
+            if next != atual+1:
+                #print(f"atual:{atual} --- next:{next} --- n:{n} --- indexInicial:{indexInicial} --- numInicial:{numInicial}")
+                print(f"({numInicial},{atual+1})")
+                newList.append((numInicial,atual+1))
+            else:
+                #print(f"atual:{atual} --- next:{next} --- n:{n} --- indexInicial:{indexInicial} --- numInicial:{numInicial}")
+                print(f"({numInicial},{next+1})")
+                newList.append((numInicial,next+1))
+    
+    return newList
+
 
 def main():
-    L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
+    L = [-4, -2, 0, 2]
     result = detect_ranges(L)
     print(L)
     print(result)
